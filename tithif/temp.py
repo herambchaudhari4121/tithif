00fbcf53354578bb8cb5114b3bc319faf059790b
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-11-06 17:36:12 +0530

    basic temp testing

--- tithif/temp.py
+++ tithif/temp.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
-from .constants import *
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+driver.get('https://www.facebook.com/')
+print("break")
